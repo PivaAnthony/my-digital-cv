12144f4d061b04520c575a9af791e1d9373eb0c2
Anthony Piva <anthony.piva@locaweb.com.br>
2024-11-21 01:51:42 -0300

    SignJWT service

--- api/app/graphql/mutations/sign_in.rb
+++ api/app/graphql/mutations/sign_in.rb
@@ -10,11 +10,6 @@ class Mutations::SignIn < Mutations::BaseMutation
   def resolve(attributes:)
     user = User.find_by!(email: attributes.email).authenticate(attributes.password)
 
-    {
-      bearer_token: JWT.encode(
-        { user_id: user.id, exp: 1.hour.from_now.to_i },
-        Rails.application.credentials.secret_key_base
-      )
-    }
+    SignJWT.call(user)
   end
 end
